16b2c60a077f226bffaa1aed5fbb196b2338ff0c
Marcos Ryan <marcosryanjesus@gmail.com>
2026-03-30 01:56:58 -0300

    Atividade 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,57 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // remove o selinho "debug",
-
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Carteira das Economias',
+void main() { 
+  runApp(const MainApp()); 
+} 
+ 
+class MainApp extends StatelessWidget { 
+  const MainApp({super.key}); 
+ 
+  @override 
+  Widget build(BuildContext context) { 
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false, //tira o selinho DEBUG, 
+ 
+      home: Scaffold( 
+        backgroundColor: Colors.grey, //Cor plano de fundo, 
+ 
+        appBar: AppBar( 
+          backgroundColor: Colors.deepPurple, //Cor do appBar,
+          title: const Text( 
+            'Meu primeiro APP', //Titulo do appBar,
             style: TextStyle(
-              fontSize: 20, //Tamanho da fonte do appBar,
-              color: Colors.white, //Cor da Fonte do appBar,
-            ),
-          ),
-          backgroundColor: Colors.lightBlueAccent, //Cor do appBar,
-          toolbarHeight: 80, //Altura do appBar,
-        ),
-
-        backgroundColor: const Color.fromARGB(255, 57, 77, 255),
-
-        body: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(
-              fontFamily: 'Arial', //Estilo da Fonte se Tiver no sistema,
-              fontSize: 32, //Tamanho da fonte,
-              fontWeight: FontWeight.bold, //Fonte em negrito,
-              color: Colors.white, //Cor da fonte,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+            fontSize: 22, //Tamanho da Fonte,
+            color: Colors.white //Cor da fonte/Texto,
+            ), 
+          ), 
+        ), 
+ 
+        body: Center( 
+          child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [ 
+              Text( 
+                'Bem vindo ao Flutter!', //Texto/titulo,
+                style: TextStyle( 
+                  fontSize: 32, //Tamanho da fonte,
+                  color: Colors.deepOrange, //Cor da fonte,
+                  fontWeight: FontWeight.bold, //Fonte em negrito,
+                  fontFamily: 'Arial', //Tipo de fonte (Se tiver no sistema),
+                ), 
+              ), 
+              SizedBox(height: 20), //Espaçamento entre objetos/childrens,
+              Text( 
+                'Calma, tudo fará sentido!', //Titulo ou Texto,
+                style: TextStyle( 
+                  fontSize: 18, //Tamanho da fonte,
+                  color: const Color.fromARGB(255, 1, 57, 71), //Cor da fonte, 
+                  fontWeight: FontWeight.bold, //Fonte em negrito,
+                  fontFamily: 'Arial', //Tipo de fonte,
+                ), 
+              ), 
+            ], 
+          ), 
+        ), 
+      ), 
+    ); 
+  } 
+} 
