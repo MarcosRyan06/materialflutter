80b98875cb8676c7a42c5caebc5f10f588952d8b
Marcos Ryan <marcosryanjesus@gmail.com>
2026-03-30 02:05:44 -0300

    Atividade 3 - Textos formatods

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,105 @@
 import 'package:flutter/material.dart';
-void main() { 
-  runApp(const MainApp()); 
-} 
- 
-class MainApp extends StatelessWidget { 
-  const MainApp({super.key}); 
- 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      debugShowCheckedModeBanner: false, //tira o selinho DEBUG, 
- 
-      home: Scaffold( 
-        backgroundColor: Colors.grey, //Cor plano de fundo, 
- 
-        appBar: AppBar( 
-          backgroundColor: Colors.deepPurple, //Cor do appBar,
-          title: const Text( 
-            'Meu primeiro APP', //Titulo do appBar,
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //Tira o selinho de debug,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlueAccent, //Cor do appBar,
+          toolbarHeight: 80, //Altura do appbar,
+          title: Text(
+            'Tentando Fazer Sozinho, Mas ta dificil', //Titulo do appBar,
             style: TextStyle(
-            fontSize: 22, //Tamanho da Fonte,
-            color: Colors.white //Cor da fonte/Texto,
-            ), 
-          ), 
-        ), 
- 
-        body: Center( 
-          child: Column( 
-            mainAxisAlignment: MainAxisAlignment.center, 
-            children: [ 
-              Text( 
-                'Bem vindo ao Flutter!', //Texto/titulo,
-                style: TextStyle( 
-                  fontSize: 32, //Tamanho da fonte,
-                  color: Colors.deepOrange, //Cor da fonte,
-                  fontWeight: FontWeight.bold, //Fonte em negrito,
-                  fontFamily: 'Arial', //Tipo de fonte (Se tiver no sistema),
-                ), 
-              ), 
-              SizedBox(height: 20), //Espaçamento entre objetos/childrens,
-              Text( 
-                'Calma, tudo fará sentido!', //Titulo ou Texto,
-                style: TextStyle( 
-                  fontSize: 18, //Tamanho da fonte,
-                  color: const Color.fromARGB(255, 1, 57, 71), //Cor da fonte, 
-                  fontWeight: FontWeight.bold, //Fonte em negrito,
-                  fontFamily: 'Arial', //Tipo de fonte,
-                ), 
-              ), 
-            ], 
-          ), 
-        ), 
-      ), 
-    ); 
-  } 
-} 
+              //Estilos do appBar,
+              color: Colors.white, //Cor da fonte,
+              fontSize: 22, //Tamanho da fonte,
+            ),
+          ),
+        ),
+
+        backgroundColor: Colors.grey, //Cor de Fundo,
+
+        body: Center(
+          child: Column( //Aqui tudo que esta no children vai ficar um em baixo do outro,
+            mainAxisAlignment: MainAxisAlignment.center, // Aqui deixa tudo centralizado na tela,
+            children: [
+              Text(
+                'Marcos Ryan Souza De Jesus', //Texto,
+                style: TextStyle(
+                  fontSize: 20, //Tamanho da Fonte,
+                  color: Colors.black, //Cor da Fonte,
+                  fontWeight: FontWeight.bold, //Fonte em NEgrito,
+                  fontStyle: FontStyle.italic, //Fonte em italico,
+                ),
+              ),
+
+              SizedBox(height: 20), // Espaçamento em altura,
+
+              Text(
+                'Desenvolvimento de Sistemas',
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.pink,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Text(
+                'Programação Mobile - Flutter',
+                style: TextStyle(
+                  fontSize: 23,
+                  color: Colors.deepPurple,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+
+              SizedBox(height: 50),
+
+              Row( // Aqui as children vão ficar lado a lodo, 
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Não desista do seus Sonhos',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+
+                  SizedBox(width: 140),
+
+                  Row(
+                    children: [
+                      Text(
+                        '2026',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
